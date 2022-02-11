07dd36dba5f88bbef2d4b9e75428a3df5f745ee3
jseiglie <javierseiglie@gmail.com>
2022-02-11 11:28:53 +0000

    files created

---
+++ src/front/js/layout.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
+
+import { Private } from "./pages/private";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Signup } from "./pages/signup";
+import injectContext from "./store/appContext";
+
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
+
+
+//create your first component
+const Layout = () => {
+	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	const basename = process.env.BASENAME || "";
+
+	return (
+		<div>
+			<BrowserRouter basename={basename}>
+				<ScrollToTop>
+					<Navbar />
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route exact path="/login">
+							<Login />
+						</Route>
+						<Route exact path="/signup">
+							<Signup />
+						</Route>
+						<Route exact path='/private'>
+							<Private />
+						</Route>
+						<Route>
+							<h1>Not found!</h1>
+						</Route>
+					</Switch>
+					<Footer />
+				</ScrollToTop>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default injectContext(Layout);
---
+++ src/front/js/pages/home.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<>
+		</>
+	);
+};
